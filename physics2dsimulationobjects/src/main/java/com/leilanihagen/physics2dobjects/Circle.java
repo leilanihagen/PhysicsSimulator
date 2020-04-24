a55940b0f04faf63d497de47be0e8217d24cd1ee
leilanihagen <hagen.leilani@gmail.com>
2020-04-23 22:54:59 -0700

    Init commit.

---
+++ physics2dsimulationobjects/src/main/java/com/leilanihagen/physics2dobjects/Circle.java
@@ -0,0 +1,21 @@
+package physics2dsimulationobjects.src.main.java.com.leilanihagen.physics2dobjects;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(String color, double mass, double locX, double locY, double radius) {
+        super(color, mass, locX, locY);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+
+    }
+    public double getRadius() {
+        return this.radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
